5999baa295f6ca647aa7de9e1d6ada6da3698d30
saramnim <12hyeji24@gmail.com>
2023-06-05 17:52:08 +0900

    create: origin files

---
+++ src/TodoSample.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoId, setTodoId] = useState(1);
+
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setTodos([
+            ...todos,
+            {
+              todoId: todoId,
+              todoText: e.target.todoText.value,
+            },
+          ]);
+          setTodoId(todoId + 1);
+        }}
+      >
+        <input type="text" />
+        <input type="submit" placeholder="할일 쓰세요" name="todoText" />
+      </form>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li key={index}>
+              {todo.todoText}
+              <span
+                onClick={() => {
+                  setTodos(
+                    todos.filter((item) => {
+                      return item.todoId !== todo.todoId;
+                    })
+                  );
+                }}
+              >
+                X
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
